9df891ec9ad90eb082bd9a000bced54334c611b1
bujosa <davidbujosa@gmail.com>
2023-06-05 03:46:31 -0400

    feat: changes example

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
 const firefly = require("firefly");
 
 async function main() {
+  // Create a new contract interface
   const contractInterface = {
-    name: "Greeter",
-    version: "1.0.0",
-    network: "testnet",
+    name: "MyContract",
+    version: 1,
+    network: "ethereum",
     connector: "hyperledger",
     functions: [
       {
-        name: "greet",
-        inputs: [],
-        outputs: [{ name: "greeting", type: "string" }],
+        name: "addOwner",
+        inputs: [
+          {
+            name: "owner",
+            type: "string",
+          },
+        ],
+        outputs: [],
       },
     ],
     events: [
       {
-        name: "greeted",
-        inputs: [{ name: "greeting", type: "string" }],
+        name: "OwnerAdded",
+        inputs: [
+          {
+            name: "owner",
+            type: "string",
+          },
+        ],
       },
     ],
   };
 
-// Create another contract using both blockchain ethereum and hyperledger, and the contract is called HelloWorld
-//   const contractInterface2 = {
-//     name: "HelloWorld",
-//     version: "1.0.0",
-//     network: "testnet",
-//     connector: "ethereum",
-//     functions: [
-//       {
-//         name: "helloWolrd",
-//         inputs: [],
-//         outputs: [{ name: "greeting", type: "string" }],
-//       },
-//     ],
-//     events: [
-//       {
-//         name: "greeted",
-//         inputs: [{ name: "greeting", type: "string" }],
-//       },
-//     ],
-//   };
+  // Broadcast the contract interface to the network
+  const response = await firefly.broadcastContractInterface(contractInterface);
+  console.log("Contract interface broadcasted successfully!", response);
 
-  const contract = await firefly.broadcastContractAInterface(contractInterface);
-  console.log(contract);
+  // Create an HTTP API for the contract
+  const api = await firefly.createApi(contractInterface);
 
-  const greeting = await contract.greet();
-  console.log(greeting);
+  // Add the owner to the contract
+  const response2 = await api.addOwner("my-owner");
+  console.log("Owner added successfully!", response2);
 
-  const event = await contract.on("greeted", (event) => {
-    console.log(event);
+  // Check that the owner was added successfully
+  const event = await firefly.waitForEvent("OwnerAdded", {
+    owner: "my-owner",
   });
+
+  console.log("Owner added successfully!");
 }
 
-main();
+main().catch(console.error);
